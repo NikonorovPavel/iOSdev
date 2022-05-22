8f0fac0a6c937390f509f71ac542e8dfd98def75
Nikonorov Pavel <nikonorov_pavel@icloud.com>
2022-05-23 01:18:29 +0300

    Homework 1.3
    Homework 1.3

---
+++ iosui3/Navigation/Navigation/FeedViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Pavel on 21.05.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    let post = Post()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+        makeButton()
+    }
+    private func makeButton() {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        button.center = view.center
+        button.setTitle("Push button", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        button.layer.cornerRadius = 3
+        view.addSubview(button)
+    }
+
+    @objc private func tapAction() {
+        let postVC = PostViewController()
+        postVC.postTitle = post.title
+        navigationController?.pushViewController(postVC, animated: true)
+    }
+}
---
+++ iosui3/Navigation/Navigation/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Pavel on 19.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
