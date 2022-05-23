b2b40298700231baa51923f41bed5522d7fba293
Nikonorov Pavel <nikonorov_pavel@icloud.com>
2022-05-23 19:53:30 +0300

    Homework 2.1
    Homework 2.1

---
+++ iosui4/Navigation/Navigation/FeedPost.swift
@@ -0,0 +1,10 @@
+//
+//  FeedPost.swift
+//  Navigation
+//
+//  Created by Pavel on 21.05.2022.
+//
+
+struct Post {
+    var title: String = "Settings"
+}
---
+++ iosui4/Navigation/Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Pavel on 21.05.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    private lazy var profileHV: ProfileHeaderView = {
+        let view = ProfileHeaderView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        profileHV.avatarImageView.layer.cornerRadius = profileHV.avatarImageView.layer.frame.width / 2
+    }
+
+    private func setupView() {
+        self.view.backgroundColor = .lightGray
+        self.view.addSubview(self.profileHV)
+
+        NSLayoutConstraint.activate([
+            self.profileHV.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.profileHV.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.profileHV.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            self.profileHV.heightAnchor.constraint(equalToConstant: 220),
+        ])
+    }
+}
+
